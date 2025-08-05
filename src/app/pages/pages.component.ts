25f4b9020f9457dd7827df0e523d9019051c128f
Diego Ivan alvarado vela <dialvaradov@alumno.unsm.edu.pe>
2025-08-05 00:49:50 -0500

    di-front-05-00:49

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import {SidebarComponent} from "../components/sidebar/sidebar.component";
 import {NavbarComponent} from "../components/navbar/navbar.component";
+import {SidebarModule} from "primeng/sidebar";
+import {ButtonModule} from "primeng/button";
+import {AnimateModule} from "primeng/animate";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-	imports: [CommonModule, RouterModule, SidebarComponent, NavbarComponent],
+	imports: [CommonModule, RouterModule, SidebarComponent, NavbarComponent,SidebarModule,ButtonModule],
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss']
 })
 export class PagesComponent {
-
+	sidebarVisible:boolean=false
 }
