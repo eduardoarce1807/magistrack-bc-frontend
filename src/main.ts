4cc87091c01df75063b09e5d95a8f0035b860de7
eduardoarce1807 <eduardoarce@upeu.edu.pe>
2025-09-08 18:16:50 -0500

    Obs 08/09

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 /// <reference types="@angular/localize" />
 
 import { bootstrapApplication } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeEsPe from '@angular/common/locales/es-PE';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+
+// Registrar los datos del locale
+registerLocaleData(localeEs);
+registerLocaleData(localeEsPe);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
